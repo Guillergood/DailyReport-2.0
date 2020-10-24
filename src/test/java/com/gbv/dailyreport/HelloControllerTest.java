e5765895dbc92412fa0128132a0c90682f9aa867
Guillermo Bueno Vargas <guillergood@correo.ugr.es>
2020-10-24 20:23:30 +0200

    Wrong test

--- src/test/java/com/gbv/dailyreport/HelloControllerTest.java
+++ src/test/java/com/gbv/dailyreport/HelloControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,9 +22,10 @@ public class HelloControllerTest {
     private MockMvc mvc;
 
     @Test
+    @RequestMapping("/hello")
     public void getHello() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+                .andExpect(content().string(equalTo("Hello World!")));
     }
 }
