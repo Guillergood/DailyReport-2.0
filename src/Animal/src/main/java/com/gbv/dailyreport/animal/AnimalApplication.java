23fb50f51ff2c5fc10a236b669c2d7b013bb6b4b
Guillermo Bueno Vargas <guillergood@correo.ugr.es>
2020-11-21 10:12:14 +0100

    Prueba de compose con mongodb en Animal #66

--- src/Animal/src/main/java/com/gbv/dailyreport/animal/AnimalApplication.java
+++ src/Animal/src/main/java/com/gbv/dailyreport/animal/AnimalApplication.java
@@ -1,7 +1,9 @@
 package com.gbv.dailyreport.animal;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
