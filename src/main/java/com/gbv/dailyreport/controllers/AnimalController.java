889e0d6a570f38bdeee408f15d75adf9e6006d1b
Guillermo Bueno Vargas <guillergood@correo.ugr.es>
2020-12-10 09:43:36 +0100

    Controlador de Animal #72 y test al 100% de coverage #41

---
+++ src/main/java/com/gbv/dailyreport/controllers/AnimalController.java
@@ -0,0 +1,96 @@
+package com.gbv.dailyreport.controllers;
+
+import com.gbv.dailyreport.model.Animal;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+public class AnimalController {
+
+
+    private final List<Animal> animals = new ArrayList<>();
+    {
+        animals.add(new Animal(1, "entity_1", false));
+        animals.add(new Animal(2, "entity_2", false));
+        animals.add(new Animal(3, "entity_3", false));
+        animals.add(new Animal(4, "entity_4", false));
+    }
+
+
+    @RequestMapping("/dailyreport/animal/all")
+    public List<Animal> findAll() {
+        return animals;
+    }
+
+    //Llamada GET que devuelve un animal
+    @GetMapping(value = "/dailyreport/animal/{id}")
+    public ResponseEntity<?> getAnimal(@PathVariable(value = "id") final int id) {
+        Animal animal;
+        try {
+            animal = animals.get(id);
+        }
+        catch (IndexOutOfBoundsException e){
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body("Not found");
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-COM-PERSIST", "true");
+
+        HttpEntity<Animal> request = new HttpEntity<>(animal, headers);
+
+
+        return ResponseEntity.ok(Objects.requireNonNull(request.getBody()));
+    }
+
+    @PutMapping(value = "/dailyreport/animal/{id}", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<?> update(@PathVariable("id") final int id, @RequestBody final Animal sourceAnimal) {
+        Animal targetAnimal;
+        try{
+            targetAnimal = animals.get(id);
+            BeanUtils.copyProperties(sourceAnimal, targetAnimal, "id");
+            return new ResponseEntity<>(targetAnimal, HttpStatus.OK);
+        }
+        catch (IndexOutOfBoundsException e){
+            return ResponseEntity
+                    .status(HttpStatus.NO_CONTENT)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body("There is not an object like that to update");
+        }
+
+
+    }
+
+    //Llamada POST que guarda un "Animal"
+    @PostMapping(path= "/dailyreport/animal", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<?> postAnimal(@RequestBody final Animal animal) {
+        animals.add(animal);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body("Added successfully");
+    }
+
+    //Llamada DELETE que elimina un "Animal"
+    @DeleteMapping(value= "/dailyreport/animal/{id}")
+    public ResponseEntity<?> deleteAnimal(@PathVariable(value= "id") final int id){
+
+        try{
+            animals.remove(animals.get(id));
+            return ResponseEntity.ok("Removed successfully");
+        }
+        catch (IndexOutOfBoundsException e){
+            return ResponseEntity
+                    .status(HttpStatus.NO_CONTENT)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body("There is not an object like that to delete");
+        }
+    }
+}
---
+++ src/test/java/com/gbv/dailyreport/AnimalControllerTest.java
@@ -0,0 +1,171 @@
+package com.gbv.dailyreport;
+
+import com.gbv.dailyreport.controllers.AnimalController;
+import com.gbv.dailyreport.model.Animal;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.Assert;
+import org.springframework.util.MimeTypeUtils;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(AnimalController.class)
+public class AnimalControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getAllAnimals() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                .get("/dailyreport/animal/all")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(4)));
+    }
+
+    @Test
+    public void getAnimals_invalidAcceptHeader_shouldReturnNotAcceptable() throws Exception {
+        // Given
+        final String invalidAcceptMimeType = MimeTypeUtils.APPLICATION_XML_VALUE;
+
+        // When
+        final ResultActions result = mvc.perform(MockMvcRequestBuilders
+                .get("/dailyreport/animal/all")
+                .accept(invalidAcceptMimeType));
+
+        // Then
+        result.andExpect(status().isNotAcceptable());
+    }
+
+    @Test
+    public void getAnimal_nonExistingAnimal_shouldReturnNotFound() throws Exception {
+        // Given
+        final int mockedId = 5;
+
+        // When
+        final ResultActions result = mvc.perform(MockMvcRequestBuilders
+                .get("/dailyreport/animal/".concat(Integer.toString(mockedId)))
+                .accept(MimeTypeUtils.APPLICATION_JSON_VALUE));
+
+        // Then
+        result.andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void getAnimal_withExistingAnimal_shouldReturnOk() throws Exception {
+        // Given
+        final int mockedId = 2;
+
+        // When
+        final ResultActions result = mvc.perform(MockMvcRequestBuilders
+                .get("/dailyreport/animal/".concat(Integer.toString(mockedId)))
+                .accept(MimeTypeUtils.APPLICATION_JSON_VALUE));
+
+        // Then
+        result.andExpect(status().isOk());
+    }
+
+    @Test
+    public void updateAnimal_withExistingAnimal_shouldReturnOk() throws Exception {
+        // Given
+        final int mockedId = 2;
+        final ResultActions sourceAnimal = mvc.perform(MockMvcRequestBuilders
+                .get("/dailyreport/animal/".concat(Integer.toString(mockedId)))
+                .accept(MimeTypeUtils.APPLICATION_JSON_VALUE));
+
+        String sourceAnimalString = sourceAnimal.andReturn().getResponse().getContentAsString();
+        Animal modifiedAnimal = new Animal(sourceAnimalString);
+        modifiedAnimal.setChecked(true);
+        modifiedAnimal.setName("changed");
+        // When
+        final ResultActions result = mvc.perform(MockMvcRequestBuilders
+                .put("/dailyreport/animal/".concat(Integer.toString(mockedId)))
+                .content(modifiedAnimal.serialize())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+
+        // Then
+        result.andExpect(status().isOk());
+    }
+
+    @Test
+    public void updateAnimal_withNoExistingAnimal_shouldReturnNoContent() throws Exception {
+        // Given
+        final int mockedId = 2;
+        final ResultActions sourceAnimal = mvc.perform(MockMvcRequestBuilders
+                .get("/dailyreport/animal/".concat(Integer.toString(mockedId)))
+                .accept(MimeTypeUtils.APPLICATION_JSON_VALUE));
+
+        String sourceAnimalString = sourceAnimal.andReturn().getResponse().getContentAsString();
+        Animal modifiedAnimal = new Animal(sourceAnimalString);
+        modifiedAnimal.setChecked(true);
+        modifiedAnimal.setName("changed");
+
+        Assert.notNull(modifiedAnimal.toString(),"Animal is null");
+
+        // When
+        final ResultActions result = mvc.perform(MockMvcRequestBuilders
+                .put("/dailyreport/animal/".concat(Integer.toString(100)))
+                .content(modifiedAnimal.serialize())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+
+        // Then
+        result.andExpect(status().isNoContent());
+    }
+
+    //Metodo para añadir un animal a la colecccion
+    @Test
+    public void addAnimal_withExistingData_shouldReturnOk() throws Exception {
+        Animal animal = new Animal(40, "Bonobo", false);
+
+        this.mvc.perform(MockMvcRequestBuilders
+                .post("/dailyreport/animal/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(animal.serialize()))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful());
+
+
+    }
+
+
+    //Metodo para eliminar un animal de la colecccion
+    @Test
+    public void delete_withExistingData_shouldReturnOk() throws Exception {
+        this.mvc.perform(MockMvcRequestBuilders
+                .delete("/dailyreport/animal/0")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful());
+
+    }
+
+    @Test
+    public void delete_withBadId_shouldReturnNoContent() throws Exception {
+        this.mvc.perform(MockMvcRequestBuilders
+                .delete("/dailyreport/animal/100")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+    }
+
+
+}
+
