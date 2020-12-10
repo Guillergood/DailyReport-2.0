75268a236cd070f08fe9c24dedb245c2e4e1953f
Guillermo Bueno Vargas <guillergood@correo.ugr.es>
2020-12-10 09:44:32 +0100

    Clase principal de la aplicación #72 y test al 100% de coverage #41

---
+++ src/main/java/com/gbv/dailyreport/DailyreportApplication.java
@@ -0,0 +1,13 @@
+package com.gbv.dailyreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyreportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyreportApplication.class, args);
+    }
+
+}
