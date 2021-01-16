6e0f53d17480e34f8cdffed77f4f0738aa62b47a
Guillermo Bueno Vargas <guillergood@correo.ugr.es>
2021-01-16 08:44:38 +0100

    Demasiada limpieza... #79 :monocle_face: :grin:

--- src/main/java/com/gbv/dailyreport/services/AnimalService.java
+++ src/main/java/com/gbv/dailyreport/services/AnimalService.java
@@ -1,4 +1,13 @@
 package com.gbv.dailyreport.services;
 
+import com.gbv.dailyreport.model.Animal;
+
+import java.util.List;
+
 public interface AnimalService {
+    void add(Animal animal);
+    void delete(Animal animal);
+    void edit(int id, Animal animal);
+    Animal get(int id);
+    List<Animal> getAll();
 }
--- src/main/java/com/gbv/dailyreport/services/CuidadorService.java
+++ src/main/java/com/gbv/dailyreport/services/CuidadorService.java
@@ -1,4 +1,13 @@
 package com.gbv.dailyreport.services;
 
+import com.gbv.dailyreport.model.Cuidador;
+
+import java.util.List;
+
 public interface CuidadorService {
+    void add(Cuidador cuidador);
+    void delete(Cuidador cuidador);
+    void edit(int id, Cuidador cuidador);
+    Cuidador get(int id);
+    List<Cuidador> getAll();
 }
