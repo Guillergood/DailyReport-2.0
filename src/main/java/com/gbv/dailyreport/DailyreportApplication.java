635dd3a1c56342bafd9c32c11a77e256a899c5f5
Guillermo Bueno Vargas <guillergood@correo.ugr.es>
2021-01-31 10:45:32 +0100

    Cambio de configuración de la aplicación para que use MongoDB #84

--- src/main/java/com/gbv/dailyreport/DailyreportApplication.java
+++ src/main/java/com/gbv/dailyreport/DailyreportApplication.java
@@ -2,12 +2,13 @@ package com.gbv.dailyreport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@EnableMongoRepositories
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class DailyreportApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DailyreportApplication.class, args);
     }
-
 }
